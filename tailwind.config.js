4c4fe8e8fa6654ecfe938e84ef34cc285ec625ff
Jovanka <samudrajovanka20@gmail.com>
2021-06-04 13:25:20 +0700

    change purge in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   purge: [
-    './src/**/*.html',
-    './src/**/*.js',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
